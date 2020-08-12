90f6206822abbc7a393828283017da0db4a14489
Simar Herrera <seherreraj@udistrital.edu.co>
2020-08-11 20:49:02 -0500

    Primer Subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TableroComponent } from './tablero/tablero.component';
+import { ComunicacionComponent } from './comunicacion/comunicacion.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableroComponent,
+    ComunicacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
